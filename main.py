d3f566fc1cebec60328998582f56a482ca3c660a
Mohamedweli <mohamedwelihassan80@gmail.com>
2026-09-15 12:21:04 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+price = float(input("Enter the price of one item: "))
+
+quantity = int(input("Enter the quantity: "))
+
+total = price * quantity
+
+print(f"{quantity} items at Ksh {price:.2f} each = Ksh {total:.2f}")
